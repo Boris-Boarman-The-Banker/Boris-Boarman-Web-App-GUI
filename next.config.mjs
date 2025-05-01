c2b3511b366f8eac0421c7be221c18624cbfe39f
boo9ieman <16269033+imboogieman@users.noreply.github.com>
2025-05-01 09:53:19 +0000

    packages mannager settings update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  poweredByHeader: false,
   images: {
     remotePatterns: [
       {
@@ -10,14 +11,18 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: true,
+  },
   rewrites: async () => {
     return [
       {
         source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+        destination: process.env.NODE_ENV === 'development'
+          ? 'http://127.0.0.1:5328/api/:path*'
+          : process.env.VERCEL_URL 
+            ? `https://${process.env.VERCEL_URL}/api/:path*`
+            : '/api/:path*',
       },
     ];
   },
